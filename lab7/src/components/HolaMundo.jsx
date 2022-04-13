f3bc2a1c2a520796b9ca58fa7498f242581578ce
stefanoaragoni <69864405+stefanoaragoni@users.noreply.github.com>
2022-04-13 11:03:47 -0600

    Add files via upload

---
+++ lab7/src/components/HolaMundo.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../assets/styles/Main.scss";
+
+const HolaMundo = () => {
+  return (
+    <div className="container">
+      <h1>Hola mundo</h1>
+    </div>
+  );
+};
+
+export default HolaMundo;
---
+++ lab7/src/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import HolaMundo from "./components/HolaMundo";
+
+ReactDOM.render(<HolaMundo />, document.getElementById("app"));
